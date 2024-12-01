f947ba7c59047e19a8d80619b9299fb526a7203d
vladislav-og <vladislav.ogorodnik@mail.ru>
2024-12-01 21:01:03 +0200

    aoc 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,10 +1,54 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val leftSideList = mutableListOf<Int>()
+        val rightSideList = mutableListOf<Int>()
+        for (row in input) {
+            val (leftNum, rightNum) = row.split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+            leftSideList.add(leftNum)
+            rightSideList.add(rightNum)
+        }
+
+        val leftSortedList = leftSideList.sorted()
+        val rightSortedList = rightSideList.sorted()
+        var distance = 0
+        for ( i in leftSideList.indices) {
+            val curValue = abs(leftSortedList.elementAt(i) - rightSortedList.elementAt(i))
+            distance += curValue
+        }
+
+        return distance
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val leftSideList = mutableListOf<Int>()
+        val rightSideList = mutableMapOf<Int, Int>()
+        for (row in input) {
+            val (leftNum, rightNum) = row.split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+            leftSideList.add(leftNum)
+            val curValue = rightSideList[rightNum]
+            if (curValue == null) {
+                rightSideList[rightNum] = 1
+            } else {
+                rightSideList[rightNum] = curValue + 1
+            }
+        }
+
+        var result = 0
+        for (num in leftSideList) {
+            if (rightSideList[num] != null) {
+                result += num * rightSideList[num]!!
+            }
+        }
+
+        return leftSideList.map {
+            if (rightSideList[it] == null) {
+                0
+            } else {
+                rightSideList[it]!! * it
+            }
+        }.sum()
     }
 
     // Test if implementation meets criteria from the description, like:
